06328a4e8a4915e2e6ed01f6a94c5fc3bda78d8c
Joseph Mate <joseph.mate@gmail.com>
2016-12-08 21:11:36 -0500

    using month class and date functions,  processing raised errors

--- amortization_controller.py
+++ amortization_controller.py
@@ -1,11 +1,14 @@
 import datetime
+from amortization_month_payment import Amort_MonthPayment
+from amortization_date_functions import AddMonths
+from amortization_date_functions import CanBeLastDayOfMonth
 
 class Amort_Controller:
     def __init__(self, amort_model, amort_view):
         self.amort_model = amort_model
         self.amort_view = amort_view
         self._dateIsLastDayOfMonth = False
-        
+      
     # never found out what NU stood for
     # this will round up the interest,
     # but only if it is more than zero 
@@ -23,10 +26,8 @@ class Amort_Controller:
     def amortize(self):
         badMonthAmount = False
         beginBalance = self.amort_model._loanAmount
-        payment = 0
-        principal = 0
-        interest = 0
         endBalance = 0
+        startDate = self.amort_model.StartDate
         # this should not be needed
         # calcPayment is set to overridePayment
         if (self.amort_model._overridePayment > 0):
@@ -34,40 +35,51 @@ class Amort_Controller:
         else:
             paymentPlanned = self.amort_model._calcPayment
         for month in range(0, self.amort_model._loanMonths):
-            if (payment > endBalance and endBalance > 0):
-                beginBalance = endBalance
-                interest = beginBalance * (self.amort_model._percent / 100) / 12
-                interest = self.calculateNU(interest)
-                payment = round(endBalance + interest, 2)
-                principal = round(payment - interest, 2)
-                endBalance = 0
+            currentMonth = Amort_MonthPayment()
+            currentMonth.Payment = 0
+            currentMonth.Principal = 0
+            currentMonth.Interest = 0
+            currentMonth.EndBalance = endBalance
+            currentMonth.PaymentDate = AddMonths(startDate, month, self._dateIsLastDayOfMonth)
+            if (currentMonth.Payment > currentMonth.EndBalance and currentMonth.EndBalance > 0):
+                currentMonth.BeginBalance = currentMonth.EndBalance
+                currentMonth.Interest = currentMonth.BeginBalance * (self.amort_model._percent / 100) / 12
+                currentMonth.Interest = self.calculateNU(currentMonth.Interest)
+                currentMonth.Payment = round(currentMonth.EndBalance + currentMonth.Interest, 2)
+                currentMonth.Principal = round(currentMonth.Payment - currentMonth.Interest, 2)
+                currentMonth.EndBalance = 0
             else:
-                beginBalance = round(beginBalance - principal, 2)
-                payment = paymentPlanned
-                interest = beginBalance * (self.amort_model._percent / 100) / 12
-                interest = self.calculateNU(interest)
+                currentMonth.BeginBalance = round(beginBalance - currentMonth.Principal, 2)
+                currentMonth.Payment = paymentPlanned
+                currentMonth.Interest = currentMonth.BeginBalance * (self.amort_model._percent / 100) / 12
+                currentMonth.Interest = self.calculateNU(currentMonth.Interest)
                 if (month == self.amort_model._loanMonths - 1 and self.amort_model._loanMonths != 1):
                     # 05-01-1992 jam - do not execute if months = 1
-                    payment = round(beginBalance + interest, 2)
-                    principal = beginBalance
+                    currentMonth.Payment = round(currentMonth.BeginBalance + currentMonth.Interest, 2)
+                    currentMonth.Principal = currentMonth.BeginBalance
                     # allow balloon payments if balance is greater than zero  11/14 jam
                     # REM IF (B@(2) > 1.1 * A@(4)) OR (B@(2) < .9 * A@(4)) THEN
                     # yes, the original code was BASIC, with poorly named variables
-                    if (abs(payment - interest - endBalance) > .01):
-                        print("payment: " + str(payment))
-                        print("interest: " + str(interest))
-                        print("end balance: " + str(endBalance))
+                    if (abs(currentMonth.Payment - currentMonth.Interest - currentMonth.EndBalance) > .01):
+                        print("month: " + str(month + 1))
+                        print("begin balance: " + str(currentMonth.BeginBalance))
+                        print("payment: " + str(currentMonth.Payment))
+                        print("interest: " + str(currentMonth.Interest))
+                        print("principal: " + str(currentMonth.Principal))
+                        print("end balance: " + str(currentMonth.EndBalance))
                         raise ValueError("Payment less interest less ending balance is greater than zero.")
                 else:
-                    principal = round(payment - interest, 2)
-                endBalance = round(beginBalance - principal, 2)
+                    currentMonth.Principal = round(currentMonth.Payment - currentMonth.Interest, 2)
+                currentMonth.EndBalance = round(currentMonth.BeginBalance - currentMonth.Principal, 2)
                 print("month: " + str(month + 1))
-                print("begin balance: " + str(beginBalance))
-                print("payment: " + str(payment))
-                print("interest: " + str(interest))
-                print("principal: " + str(principal))
-                print("end balance: " + str(endBalance))
-                if (endBalance < 0):
+                print("date: " + str(currentMonth.PaymentDate))
+                print("begin balance: " + str(currentMonth.BeginBalance))
+                print("payment: " + str(currentMonth.Payment))
+                print("interest: " + str(currentMonth.Interest))
+                print("principal: " + str(currentMonth.Principal))
+                print("end balance: " + str(currentMonth.EndBalance))
+                beginBalance = endBalance = currentMonth.EndBalance
+                if (currentMonth.EndBalance < 0):
                     raise ValueError("Ending balance is less than zero.")          
 
     # if percent, loan amount and number months are set
@@ -94,17 +106,7 @@ class Amort_Controller:
     # ask if all dates are on the last day of the month
     def CheckForLastDayOfMonth(self, dateEntered):
         self._dateIsLastDayOfMonth = False
-        canBeLastDayOfMonth = False
-        myDate = datetime.datetime.strptime(dateEntered, '%Y-%m-%d')
-        if myDate.month == 2:
-            if myDate.year % 4 == 0:
-                canBeLastDayOfMonth = (myDate.day == 29)
-            else:
-                canBeLastDayOfMonth = (myDate.day == 28)
-        elif myDate.month == 4 or myDate.month == 6 or myDate.month == 9 or myDate.month == 11:
-            canBeLastDayOfMonth = (myDate.day == 30)
-        else:
-            canBeLastDayOfMonth = (myDate.day == 31)
+        canBeLastDayOfMonth = CanBeLastDayOfMonth(dateEntered)
         if canBeLastDayOfMonth == True:
             response = self.amort_view.promptUserLastDayOfMonth()
             # here response is True or False
@@ -129,30 +131,50 @@ class Amort_Controller:
                 response = self.amort_view.promptUser('Enter %s' % self.amort_view.LABEL_LINE_2)
                 if self.CheckForQuit(response) == True:
                     break
-                self.amort_model.StartDate = response
-                if (self.amort_model.StartDate == response):
-                    self.CheckForLastDayOfMonth(self.amort_model.StartDate)
+                try:
+                    self.amort_model.StartDate = response
+                    if (self.amort_model.StartDate == response):
+                        self.CheckForLastDayOfMonth(self.amort_model.StartDate)
+                        self.calculateMonthlyPayment()
+                except ValueError as ve:
+                    try:
+                        print (ve.args[0])
+                    except:
+                        print(str(response) + " is not a valid start date (YYYY-MM-DD).")
             elif response[:1] == '3':
                 response = self.amort_view.promptUser('Enter %s' % self.amort_view.LABEL_LINE_3)
                 if self.CheckForQuit(response) == True:
                     break
-                self.amort_model.Months = response
-                self.calculateMonthlyPayment()
+                try:
+                    self.amort_model.Months = response
+                    self.calculateMonthlyPayment()
+                except ValueError:
+                    print(str(response) + " is not a valid number for months (minimum of 1).")
             elif response[:1] == '4':
                 response = self.amort_view.promptUser('Enter %s' % self.amort_view.LABEL_LINE_4)
                 if self.CheckForQuit(response) == True:
                     break
-                self.amort_model.Amount = response
-                self.calculateMonthlyPayment()
+                try:
+                    self.amort_model.Amount = response
+                    self.calculateMonthlyPayment()
+                except ValueError:
+                    print(str(response) + " is not a valid number for amount (minimum of 0.01).")
             elif response[:1] == '5':
                 response = self.amort_view.promptUser('Enter %s' % self.amort_view.LABEL_LINE_5)
                 if self.CheckForQuit(response) == True:
                     break
-                self.amort_model.APR = response
-                self.calculateMonthlyPayment()
+                try:
+                    self.amort_model.APR = response
+                    self.calculateMonthlyPayment()
+                except ValueError:
+                    print(str(response) + " is not a valid number for percentage (0.0000 to 100.0000).")
             elif response[:1] == '6':
                 response = self.amort_view.promptUser('Enter %s' % self.amort_view.LABEL_LINE_7)
                 if self.CheckForQuit(response) == True:
                     break
-                self.amort_model.Override = response
-                self.calculateMonthlyPayment()
+                try:
+                    self.amort_model.Override = response
+                    self.calculateMonthlyPayment()
+                except ValueError:
+                    print (str(response) + " is not a valid number for payment override (minimum of 0.01).")
+
