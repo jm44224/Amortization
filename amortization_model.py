496c14b640e9e750c39c99bca771034bf1aa770e
Joseph Mate <joseph.mate@gmail.com>
2016-12-08 21:08:56 -0500

    using RaiseError to pass issues to controller

--- amortization_model.py
+++ amortization_model.py
@@ -1,5 +1,5 @@
-
 import datetime
+import amortization_date_functions
 
 class Amort_Model:
     def __init__ (self):
@@ -11,19 +11,6 @@ class Amort_Model:
         self._percent = 0.0000
         self._calcPayment = 0.00
         self._overridePayment = 0.00
-
-    # validates that a given date is in the correct format
-    # validates that a given date is actually a date
-    def validateDate(self, date_text):
-        try:
-            datetime.datetime.strptime(date_text, '%Y-%m-%d')
-            return True
-        except ValueError as ve:
-            if ve.args[0].find('does not match format') != -1:
-                print("You must enter a date in the format of YYYY-MM-DD.")
-            elif ve.args[0].find('out of range for month') != -1:
-                print("You must enter a valid date.")
-        return False
     
     # get / set Loan Amount, validate as a positive number
     @property
@@ -31,44 +18,56 @@ class Amort_Model:
         return self._loanAmount
     @Amount.setter
     def Amount(self, value):
-        if float(value) >=0.01:
-            self._loanAmount  = round(float(value), 2)
-        else:
-            print(str(value) + " is not a valid number for amount (minimum of 0.01).")
-
+        try:
+            if float(value) >=0.01:
+                self._loanAmount  = round(float(value), 2)
+            else:
+                raise ValueError
+        except ValueError:
+            raise ValueError
+        
     # get / set Annual Percentage Rate, validate as a positive percentage         
     @property
     def APR(self):
         return self._percent
     @APR.setter
     def APR(self, value):
-        if float(value) >=0.0 and float(value) <=100.0:
-            value = round(float(value), 4)
-            self._percent = float(value)
-        else:
-            print(str(value) + " is not a valid number for percentage (0.0000 to 100.0000).")
-
+        try:
+            if float(value) >=0.0 and float(value) <= 100.0:
+                value = round(float(value), 4)
+                self._percent = float(value)
+            else:
+                raise ValueError
+        except ValueError:
+            raise ValueError
+        
     # get / set Months of Loan, validate as a positive integer          
     @property
     def Months(self):
         return self._loanMonths
     @Months.setter
     def Months(self, value):
-        if int(value) >=1:
-            self._loanMonths = int(value)
-        else:
-            print(str(value) + " is not a valid number for months (minimum of 1).")
-
+        try:
+            if int(value) >=1:
+                self._loanMonths = int(value)
+            else:
+                raise ValueError
+        except ValueError:
+            raise ValueError
+        
     # get / set Monthly Payment Override, validate as a positive number
     @property
     def Override(self):
         return self._overridePayment
     @Override.setter
     def Override(self, value):
-        if float(value) >=0.01:
-            self._overridePayment  = round(float(value), 2)
-        else:
-            print(str(value) + " is not a valid number for payment override (minimum of 0.01).")
+        try:
+            if float(value) >=0.01:
+                self._overridePayment  = round(float(value), 2)
+            else:
+                raise ValueError
+        except ValueError:
+            raise ValueError
 
     # get / set Start Date of Payment, validate as a valid date format
     # this is not yet used            
@@ -77,11 +76,12 @@ class Amort_Model:
         return self._startDate
     @StartDate.setter
     def StartDate(self, value):
-        if self.validateDate(value) == True:
-            self._startDate = value
-        else:
-            print(str(value) + " is not a valid start date (YYYY-MM-DD).")
-
+        try:
+            if amortization_date_functions.ValidateDate(value) == True:
+                self._startDate = value
+        except ValueError:
+            raise 
+        
     # get / set Loan Title
     # this is not yet used    
     @property
