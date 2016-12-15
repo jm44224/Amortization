39d1ffc0f27ba8a37106a6ad707b3df84082fdf8
Joseph Mate <joseph.mate@gmail.com>
2016-12-15 18:41:57 -0500

    minor comments

--- amortization.py
+++ amortization.py
@@ -2,8 +2,10 @@ from amortization_view import Amort_View
 from amortization_model import Amort_Model
 from amortization_controller import Amort_Controller
 
+# initializes model and view, and passes to controller
 amort_model = Amort_Model()
 amort_view = Amort_View()
 amort_controller = Amort_Controller(amort_model, amort_view)
+# starts the amortization user interface
 amort_controller.RunAmort()
 
